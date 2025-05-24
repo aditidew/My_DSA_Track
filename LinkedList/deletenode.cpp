9762a2547c3e06ecab461bbb83b27ab1d0bac24e
aditidew <aditidew24@gmail.com>
2025-05-24 12:43:54 +0530

    new files are added

--- LinkedList/deletenode.cpp
+++ LinkedList/deletenode.cpp
@@ -61,6 +61,34 @@ void deleteTail(Node*& head, Node*& tail) {
 }
 
 
+void deleteAtK(Node* head, int k){
+    if(head== nullptr) return;
+    if( k==1){
+        Node* temp=head;
+        head=head->next;
+        free(temp);
+        
+
+
+    }
+     Node* temp=head;
+     int cnt=0; 
+    Node* prev= nullptr;
+    while(temp!=nullptr){
+        cnt++;
+       
+        if(cnt==k){
+            prev->next= prev->next->next;
+            free(temp);
+            break;
+
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+}
+
+
 
 void deleteHead(Node * &head, Node* &tail){
     if(head== nullptr) return;
@@ -84,6 +112,7 @@ void deleteHead(Node * &head, Node* &tail){
 int main() {
     Node* head = nullptr;
     Node* tail = nullptr;
+    int k=3;
 
     append(head, tail, 10);
     append(head, tail, 20);
@@ -98,8 +127,12 @@ int main() {
     cout << "After deleting tail: ";
     display(head);
 
-    deleteHead(head, tail);
-    cout << "After deleting head ";
+    // deleteHead(head, tail);
+    // cout << "After deleting head ";
+    // display(head);
+
+    deleteAtK(head, k);
+    cout << "After deleting at "<< k <<"th position \n" ;
     display(head);
 
     return 0;
