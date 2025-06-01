2e7e6e708bb4562309416f50e429c166cec72ce4
aditidew <aditidew24@gmail.com>
2025-06-01 21:08:35 +0530

    new files added

---
+++ LinkedList/sortLL.cpp
@@ -0,0 +1,20 @@
+                                
+#include <iostream>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+// Node class represents a
+// node in a linked list
+class Node {
+public:
+    
+    int data;
+    Node* next;
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
